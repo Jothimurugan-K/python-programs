2dc6dd2c1ba250ee6eef1c0126490ef7a6bedb41
Jothimurugan-K <jothimurugan.krishnasamy@gmail.com>
2024-07-01 17:08:33 +0530

    print character presenr at even odd index seperately

--- string_programs/print_character_present_at_even_odd_index_seperately.py
+++ string_programs/print_character_present_at_even_odd_index_seperately.py
@@ -1,5 +1,6 @@
 str = 'jothimurugan'
 
+# Method 2
 count = 0
 print("Odd index characters...")
 for x in str:
@@ -12,3 +13,10 @@ for x in str:
     if count%2 != 0:
         print(x)
     count = count + 1
+
+# Method 2
+odd_char = str[0::2]
+even_char = str[1::2]
+
+print(odd_char)
+print(even_char)
